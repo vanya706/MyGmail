af06e121dfcb1121f6f5977e14017f6dbd98376a
vanya706 <vanya.mostoviy@gmail.com>
2021-01-06 01:08:24 +0200

    init project

---
+++ src/main/java/com/ivanmostovyi/demo/MyGmailApplication.java
@@ -0,0 +1,13 @@
+package com.ivanmostovyi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGmailApplication.class, args);
+    }
+
+}
