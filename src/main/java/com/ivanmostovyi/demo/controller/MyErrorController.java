a7d321509a70382b036f60de7a92b66487593a0d
vanya706 <vanya.mostoviy@gmail.com>
2021-01-06 15:08:56 +0200

    some edits

--- src/main/java/com/ivanmostovyi/demo/controller/MyErrorController.java
+++ src/main/java/com/ivanmostovyi/demo/controller/MyErrorController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
 
 @Controller
 public class MyErrorController implements ErrorController {
@@ -15,13 +14,10 @@ public class MyErrorController implements ErrorController {
     public String handleError(Model model, HttpServletRequest request){
 
         Integer errorCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+        Exception errorMessage = (Exception) request.getAttribute("javax.servlet.error.exception");
 
-        model.addAttribute("error_number", errorCode);
-
-        if (exception != null){
-            model.addAttribute("exception", exception.getMessage());
-        }
+        model.addAttribute("errorCode", errorCode);
+        model.addAttribute("errorMessage", errorMessage.getMessage());
 
         return "error/show";
     }
